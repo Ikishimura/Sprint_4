9f0a72560a520b650cc9b3d68a0e122b65d7fb60
Ikishimura <avatarus2010@yandex.ru>
2022-02-13 09:58:56 +0300

    comlete

---
+++ src/test/java/TestPositiveChecksAccount.java
@@ -0,0 +1,31 @@
+import io.qameta.allure.junit4.DisplayName;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(Parameterized.class)
+public class TestPositiveChecksAccount {
+    private String name;
+
+    public TestPositiveChecksAccount(String name){
+        this.name=name;
+    }
+
+    @Parameterized.Parameters(name = "Test with negative checks = {0} {1} {2} {3}")
+    public static Object [][] getNamePositiveTest(){
+        return new Object[][]{
+                {"1 3"},
+                {"123456789 123456789"},
+                {"Тимоти Шаламе"},
+                {"Timoty Shalome"}
+        };
+    }
+    @Test
+    @DisplayName("Test with negative checks")
+    public void namePositiveTest(){
+     Account account = new Account(name);
+     boolean actual = account.checkNameToEmboss();
+     assertTrue(actual);
+    }
+}
