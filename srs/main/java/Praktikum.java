542a3300af77bd3d9881991a4c891965f022015d
Ikishimura <93490701+Ikishimura@users.noreply.github.com>
2022-02-14 19:27:32 +0300

    Delete srs/main/java directory

--- srs/main/java/Praktikum.java
+++
@@ -1,8 +0,0 @@
-public class Praktikum {
-
-    public static void main(String[] args) {
-        String name="Тимоти Шаламев";
-        Account account = new Account(name);
-        account.checkNameToEmboss();
-    }
-}
